34595b69952a1668999d2c3c2c1e77fd5bdcaa57
MisterParker <silentninja.p@yahoo.com>
2022-02-11 13:25:33 +0530

    inference on webcam video done; next item is creating a custom classifier

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let net;
+const webCamElement = document.getElementById('webcam');
 
 async function mlApp(){
     console.log('Loading the neural net (MobileNet in this case)...');
@@ -7,10 +8,30 @@ async function mlApp(){
     net = await mobilenet.load()
     console.log('Succefully loaded model...');
 
+    console.log('classifying the static image....')
     // load your image here & make predictions throght the above model
     const imgIn = document.getElementById('img');
-    const result = await net.classify(imgIn);
-    console.log(result);
+    const result_img = await net.classify(imgIn);
+    console.log(result_img);
+
+    console.log('classifying the video feed....')
+    const webCam = await tf.data.webcam(webCamElement);
+    while(true) {
+        const imgVid = await webCam.capture();
+        const result_vid = await net.classify(imgVid);
+        
+        document.getElementById('console').innerText = `
+            prediction: ${result_vid[0].className}\n
+            probability: ${result_vid[0].probability}
+        `;
+
+        // dispose the tensor to release the memory
+        imgVid.dispose();
+
+        // add buffer before capturing the next frame
+        await tf.nextFrame();
+
+    }
 }
 
 mlApp();
